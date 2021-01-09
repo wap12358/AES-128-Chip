8504f68d75f716a915422ab11d8e9510f50d67f0
Dignity-ghost <junwei.work@qq.com>
2021-01-09 16:32:40 +0800

    synfifo rtl tst modelsim pro

--- verifyPlatform/rtl/datagenerator/synfifo/rtl/synfifo.v
+++ verifyPlatform/rtl/datagenerator/synfifo/rtl/synfifo.v
@@ -7,11 +7,11 @@
 
 `timescale 1ns/1ps
 
-`define fifo_width 128;
-`define fifo_depth 8;
-`define fifo_addr 3;
+`define fifo_width 128
+`define fifo_depth 8
+`define fifo_addr 3
 
-module synfifo(clk,rst
+module synfifo(clk,rst,
                wr_en,rd_en,
                data_in,data_out,
                empty,full);
@@ -24,17 +24,17 @@ module synfifo(clk,rst
 input clk;
 input rst;
 input wr_en,rd_en;
-input  [fifo_width-1'b1:0] data_in;
+input  [`fifo_width-1'b1:0] data_in;
 output wire empty,full;
-output wire [fifo_width-1'b1:0] data_out;
+output wire [`fifo_width-1'b1:0] data_out;
 
-reg [fifo_width-1'b1:0] data_out_reg;
+reg [`fifo_width-1'b1:0] data_out_reg;
 assign data_out = data_out_reg;
 
 //Define signals:
-reg    [fifo_width-1'b1:0] ram [fifo_depth-1'b1:0];
-reg    [fifo_addr-1'b1:0] wr_ptr,rd_ptr;
-reg    [fifo_addr-1'b1:0] counter;
+reg    [`fifo_width-1'b1:0] ram [`fifo_depth-1'b1:0];
+reg    [`fifo_addr-1'b1:0] wr_ptr,rd_ptr;
+reg    [`fifo_addr-1'b1:0] counter;
 
 
 //Edit code:
@@ -57,18 +57,18 @@ end
 2'b01: begin
     data_out_reg <= ram[rd_ptr];
     counter <= counter - 1'b1;
-    rd_ptr <= (rd_ptr == fifo_depth-1'b1)?'d0:rd_ptr+1'b1;
+    rd_ptr <= (rd_ptr == `fifo_depth-1'b1)?'d0:rd_ptr+1'b1;
 end
 2'b10: begin
 	ram[wr_ptr] <= data_in;
     counter <= counter + 1'b1;
-	wr_ptr <= (wr_ptr == fifo_depth-1'b1)?'d0:wr_ptr+1'b1;
+	wr_ptr <= (wr_ptr == `fifo_depth-1'b1)?'d0:wr_ptr+1'b1;
 end
 2'b11: begin
     data_out_reg <= ram[rd_ptr];
-    rd_ptr <= (rd_ptr == fifo_depth-1'b1)?'d0:rd_ptr+1'b1;
+    rd_ptr <= (rd_ptr == `fifo_depth-1'b1)?'d0:rd_ptr+1'b1;
 	ram[wr_ptr] <= data_in;
-	wr_ptr <= (wr_ptr == fifo_depth-1'b1)?'d0:wr_ptr+1'b1;
+	wr_ptr <= (wr_ptr == `fifo_depth-1'b1)?'d0:wr_ptr+1'b1;
     counter <= counter;
 end
 default: begin
@@ -84,7 +84,7 @@ end //the end of always
 
 
 assign empty = (counter == 'd0)?1'b1:1'b0;
-assign full = (counter == fifo_depth-1'b1)?1'b1:1'b0;
+assign full = (counter == `fifo_depth-1'b1)?1'b1:1'b0;
 
 
 endmodule
---
+++ verifyPlatform/rtl/datagenerator/synfifo/rtl/synfifo_tst.v
@@ -0,0 +1,66 @@
+//File name  :    synfifo_tst.v
+//Author     :    xiaocuicui
+//Time       :    2021/01/09 16:11:21
+//Version    :    V1.0
+//Abstract   :        
+
+
+`timescale 1ns/1ps
+
+`define fifo_width 128
+`define fifo_depth 8
+`define fifo_addr 3
+
+
+module synfifo_tst();
+
+reg clk,rst;
+reg wr_en,rd_en;
+reg [`fifo_width-1'b1:0] data_in;
+wire empty,full;
+wire [`fifo_width-1'b1:0] data_out;
+
+synfifo i1(
+    .clk(clk),
+    .rst(rst),
+    .wr_en(wr_en),
+    .rd_en(rd_en),
+    .data_in(data_in),
+    .data_out(data_out),
+    .empty(empty),
+    .full(full)
+);
+
+
+initial
+begin
+clk = 0;
+rst = 0;
+data_in = 'd0;  
+wr_en = 0;
+rd_en = 0;
+$display("Running testbench");
+#20
+rst = 1; 
+#50
+wr_en = 1;
+#500
+wr_en = 0;
+rd_en = 1;
+#500
+wr_en = 0;
+rd_en = 0;
+#100
+wr_en = 1;
+rd_en = 1;
+#500
+$display("Finish testbench");
+$stop;            
+end
+
+
+always #5 clk = ~clk;    
+always #20 data_in = data_in + 1'b1;
+
+endmodule
+
