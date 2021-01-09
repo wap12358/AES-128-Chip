d44f0cf871634456c184e9ac84827cc268fb642f
wap12358 <jinhao0521@outlook.com>
2021-01-09 15:25:26 +0800

    scoreboard

--- verifyPlatform/rtl/controller/scoreboard.v
+++ verifyPlatform/rtl/controller/scoreboard.v
@@ -8,28 +8,34 @@
 module scoreboard(
     clk, rst_n,
     total, correct,
-    chip
+    chip_result, chip_en,
+    generator_result, generator_require
 );
 
 //Define pins:
-input   clk, rst_n;
-
+input                   clk, rst_n;
+output reg  [ 31: 0]    total, correct;
+input       [127: 0]    chip_result, generator_result;
+input                   chip_en;
+output                  generator_require;
 
 //Define signals:
-
-
-//Define parameters:
+wire                    equal;
 
 
 //Edit code:
+assign generator_require = chip_en;
+assign equal = (chip_result == generator_result);
+
 always@(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
-
+        total <= 32'h0;
+        correct <= 32'h0;
     end else begin
-
-
-
-
+        if (chip_en)begin
+           total <= total + 1'b1;
+           correct <= equal ? correct + 1'b1 : correct;
+        end
     end //the end of biggest if
 end //the end of always
 
