c80e75519ab4687ee11df39275f81987f74a0abf
wap12358 <jinhao0521@outlook.com>
2021-01-11 14:21:13 +0800

    modify the simulation structure to eliminate redundancy

--- verifyPlatform/tb/tb_pureHardwareVerify_top.v
+++ verifyPlatform/tb/tb_pureHardwareVerify_top.v
@@ -7,11 +7,13 @@
 
 module tb_pureHardwareVerify_top();
 
-//Define pins:
-reg   clk, rst_n, work;
+// define stimulate
+reg   clk, rst_n, work, enc;
 
 
 //Define signals:
+wire    [ 8: 0]     aes_tx, aes_rx;
+wire                clk_chip, rst_n_chip;
 
 
 //Define parameters:
@@ -24,6 +26,7 @@ initial begin
     clk = 1'b0;
     rst_n = 1'b0;
     work = 1'b0;
+    enc = 1'b1;
     #1003
     rst_n = 1'b1;
     #50003
@@ -31,16 +34,33 @@ initial begin
 
 end
 
-top top(
+platformTop#(
+    .CLK_FREQ(50_000_000),
+    .CHIP_CLK_FREQ(1_000_000),
+    .AES_TX_FREQ(50_000)
+)platformTop(
     .clk(clk),
     .rst_n(rst_n),
+    .clk_chip(clk_chip),
+    .rst_n_chip(rst_n_chip),
     .work(work),
-    .enc(1'b0),
+    .enc(enc),
     .total(),
-    .correct()
+    .correct(),
+    .aes_tx(aes_tx),
+    .aes_rx(aes_rx)
 );
 
-
+aes_top aes_top(
+    .clk(clk_chip), 
+    .rst(rst_n_chip),
+    .cu(1'b0),
+    .id(enc),
+	.user_data(aes_tx[7:0]),
+    .shi(aes_tx[8]),
+    .chip_data(aes_rx[7:0]),
+    .sho(aes_rx[8])
+);
 
 
 
